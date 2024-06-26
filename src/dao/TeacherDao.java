c9b649c44321e2c8ca9afc331d7642fcffa7ce7f
yugyeong75 <2582636245@qq.com>
2024-06-26 11:23:36 +0900

    /

--- src/dao/TeacherDao.java
+++ src/dao/TeacherDao.java
@@ -4,51 +4,63 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
+import bean.School;
 import bean.Teacher;
 
-public class TeacherDao extends DAO{
-	public Teacher get(String id) throws Exception{
-		Teacher teacher = null;
-		Connection con = getConnection();
-		PreparedStatement st;
-		st=con.prepareStatement("select * from teacher where id=?");
-		st.setString(1, id);
-		ResultSet rs=st.executeQuery();
+public class TeacherDao extends DAO {
+    public Teacher get(String id) throws Exception {
+        Teacher teacher = null;
+        Connection con = getConnection();
+        PreparedStatement st;
+        st = con.prepareStatement("select * from teacher where id=?");
+        st.setString(1, id);
+        ResultSet rs = st.executeQuery();
 
-		if(rs.next()){
-			teacher=new Teacher();
-			teacher.setId(rs.getString("id"));
-			teacher.setName(rs.getString("name"));
-			teacher.setPassword(rs.getString("password"));
-			teacher.setSchool(rs.getSchool("school_cd"));
-		}
+        if (rs.next()) {
+            teacher = new Teacher();
+            teacher.setId(rs.getString("id"));
+            teacher.setName(rs.getString("name"));
+            teacher.setPassword(rs.getString("password"));
 
-		st.close();
-		con.close();
-		return teacher ;
-	}
+            // school_cd を取得して School オブジェクトに設定
+            School school = new School();
+            school.setCd(rs.getString("school_cd"));
+            // 必要に応じて他の属性も設定します
+            teacher.setSchool(school);
+        }
 
-	public Teacher login(String id, String password) throws Exception{
-		Teacher teacher = null;
-		Connection con = getConnection();
-		PreparedStatement st;
-		st=con.prepareStatement("select * from teacher where id=? and password=?");
-		st.setString(1, id);
-		st.setString(2, password);
-		ResultSet rs=st.executeQuery();
+        rs.close();
+        st.close();
+        con.close();
+        return teacher;
+    }
 
-		if(rs.next()){
-			teacher = new Teacher();
-			teacher.setId(rs.getString("id"));
-			teacher.setName(rs.getString("name"));
-			teacher.setPassword(rs.getString("password"));
-			teacher.setSchool(rs.getSchool("school_cd"));
-		}
+    public Teacher login(String id, String password) throws Exception {
+        Teacher teacher = null;
+        Connection con = getConnection();
+        PreparedStatement st;
+        st = con.prepareStatement("select * from teacher where id=? and password=?");
+        st.setString(1, id);
+        st.setString(2, password);
+        ResultSet rs = st.executeQuery();
 
-		rs.close();
-		st.close();
-		con.close();
+        if (rs.next()) {
+            teacher = new Teacher();
+            teacher.setId(rs.getString("id"));
+            teacher.setName(rs.getString("name"));
+            teacher.setPassword(rs.getString("password"));
 
-		return teacher;
-	}
+            // school_cd を取得して School オブジェクトに設定
+            School school = new School();
+            school.setCd(rs.getString("school_cd"));
+            // 必要に応じて他の属性も設定します
+            teacher.setSchool(school);
+        }
+
+        rs.close();
+        st.close();
+        con.close();
+
+        return teacher;
+    }
 }
