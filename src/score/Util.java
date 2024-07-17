4837d1eeec8d2edd11ae8c19cfa7816d9841c5a6
k_izawa <k_izawa@192.168.227.144>
2024-07-17 10:23:56 +0900

    ゆーてぃる

--- src/score/Util.java
+++ src/score/Util.java
@@ -1,12 +1,17 @@
 package score;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import bean.School;
+import bean.Student;
 import bean.Subject;
 import bean.Teacher;
+import dao.StudentDao;
 import dao.SubjectDao;
 
 public class Util {
@@ -23,10 +28,24 @@ public class Util {
             List<Subject> subjects = subjectDao.filter(teacher.getSchool());
             request.setAttribute("subjects", subjects);
         }
+        if (teacher != null) {
+            School school = teacher.getSchool();
+            StudentDao studentDao = new StudentDao();
+            List<Student> students = studentDao.filter(school, true); // 通学中の学生をフィルタ
+            List<String> classNums = students.stream().map(Student::getClassNum).distinct().collect(Collectors.toList());
+            request.setAttribute("class_num_set", classNums);
+        }
     }
 
-    public static void setEntYearSet(HttpServletRequest request) {
-        // 実装内容
+    public static void setEntYearSet(HttpServletRequest request) throws Exception {
+        Teacher teacher = getUser(request);
+        if (teacher != null) {
+            School school = teacher.getSchool();
+            StudentDao studentDao = new StudentDao();
+            List<Student> students = studentDao.filter(school, true); // 通学中の学生をフィルタ
+            List<Integer> entYears = students.stream().map(Student::getEntYear).distinct().collect(Collectors.toList());
+            request.setAttribute("ent_year_set", entYears);
+        }
     }
 
     public static void setSubjects(HttpServletRequest request) throws Exception {
@@ -39,6 +58,6 @@ public class Util {
     }
 
     public static void setNumSet(HttpServletRequest request) {
-        // 実装内容
+        request.setAttribute("nums", Arrays.asList(1, 2)); // 回数は1, 2を設定
     }
 }
